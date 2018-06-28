7da9ae69de2c7b3205bea77f5d69f6d55e7f1b4d
Dawid Pośliński <dposlinski@gmail.com>
2018-06-28 08:45:06 +0200

    Fix unit component tests

--- tests/unit/components/step-test.js
+++ tests/unit/components/step-test.js
@@ -7,64 +7,64 @@ module('Unit | Component | Step', function(hooks) {
   module('Step', function(){
     test('does render with 0 as default', function(assert) {
       let component = this.owner.factoryFor('component:intro-js/step').create();
-      assert.equal(component['data-step'], '0');
+      assert.equal(component.get('data-step'), '0');
     });
 
     test('does render with a custom value', function(assert) {
       let component = this.owner.factoryFor('component:intro-js/step').create({ step: 4 });
-      assert.equal(component['data-step'], '4');
+      assert.equal(component.get('data-step'), '4');
     });
   });
 
   module('Intro', function() {
     test('does render with a custom value', function(assert) {
       let component = this.owner.factoryFor('component:intro-js/step').create({ intro: 'My text' });
-      assert.equal(component['data-intro'], 'My text');
+      assert.equal(component.get('data-intro'), 'My text');
     });
   });
 
   module('Tooltip class', function() {
     test('does render with a custom value', function(assert) {
       let component = this.owner.factoryFor('component:intro-js/step').create({ tooltipClass: 'my-class' });
-      assert.equal(component['data-tooltipClass'], 'my-class');
+      assert.equal(component.get('data-tooltipClass'), 'my-class');
     });
   });
 
   module('Highlight class', function() {
     test('does render with a custom value', function(assert) {
       let component = this.owner.factoryFor('component:intro-js/step').create({ highlightClass: 'my-class' });
-      assert.equal(component['data-highlightClass'], 'my-class');
+      assert.equal(component.get('data-highlightClass'), 'my-class');
     });
   });
 
   module('Position', function() {
     test('does render with bottom as default', function(assert) {
       let component = this.owner.factoryFor('component:intro-js/step').create();
-      assert.equal(component['data-position'], 'bottom');
+      assert.equal(component.get('data-position'), 'bottom');
     });
 
     test('does render with a custom value', function(assert) {
       let component = this.owner.factoryFor('component:intro-js/step').create({ position: 'top' });
-      assert.equal(component['data-position'], 'top');
+      assert.equal(component.get('data-position'), 'top');
     });
   });
 
   module('Hint', function() {
     test('does render with a custom value', function(assert) {
       let component = this.owner.factoryFor('component:intro-js/step').create({ hint: 'My text' });
-      assert.equal(component['data-hint'], 'My text');
+      assert.equal(component.get('data-hint'), 'My text');
     });
   });
 
   module('Hint position', function() {
     test('does render with top-middle as default', function(assert) {
       let component = this.owner.factoryFor('component:intro-js/step').create();
-      assert.equal(component['data-hintPosition'], 'top-middle');
+      assert.equal(component.get('data-hintPosition'), 'top-middle');
     });
 
     test('does render with a custom value', function(assert) {
       let component = this.owner.factoryFor('component:intro-js/step').create({ hintPosition: 'top' });
-      assert.equal(component['data-hintPosition'], 'top');
+      assert.equal(component.get('data-hintPosition'), 'top');
     });
   });
 });
